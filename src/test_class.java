ac6ff6e1afff0833e150f132eb02a314b1ef253c
sevilturan <100837278+sevilturan@users.noreply.github.com>
2022-03-24 23:13:53 +0300

    Update test_class.java

--- src/test_class.java
+++ src/test_class.java
@@ -2,6 +2,8 @@ public class test_class {
 
     public static void main(String[] args) {
         System.out.println("message from git");
+         System.out.println("message from github");
+        
 
     }
 }
